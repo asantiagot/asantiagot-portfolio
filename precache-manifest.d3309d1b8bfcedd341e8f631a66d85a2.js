e06ca20fbcf8d54044f82f4b41a51f3ab0f0b9bf
Jose Santiago <josantorres93@gmail.com>
2020-03-21 22:10:59 -0600

    Updates

---
+++ precache-manifest.d3309d1b8bfcedd341e8f631a66d85a2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "078d060289cfb621a1db6b468d3cd14c",
+    "url": "/asantiagot-portfolio/index.html"
+  },
+  {
+    "revision": "5060ece1a65a506c8610",
+    "url": "/asantiagot-portfolio/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "e6258316580dda092853",
+    "url": "/asantiagot-portfolio/static/js/2.fcda901d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/asantiagot-portfolio/static/js/2.fcda901d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5060ece1a65a506c8610",
+    "url": "/asantiagot-portfolio/static/js/main.cbdd9cc5.chunk.js"
+  },
+  {
+    "revision": "d366abebee4109971b2f",
+    "url": "/asantiagot-portfolio/static/js/runtime-main.8e0f0f5f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/asantiagot-portfolio/static/media/logo.5d5d9eef.svg"
+  }
+]);
